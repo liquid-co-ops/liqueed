4e30a7011f75e4928cecf6c10d37b544c1f47ec1
ajlopez <webmaster@ajlopez.com>
2014-07-24 17:06:42 -0300

    Add person to project, first tests

--- services/project.js
+++ services/project.js
@@ -2,6 +2,7 @@
 var ostore = require('ostore');
 
 var store = ostore.createStore();
+var peoplestore = ostore.createStore();
 
 function addProject(data) {
     return store.add(data);
@@ -11,8 +12,18 @@ function getProjectById(id) {
     return store.get(id);
 }
 
+function addPersonToProject(projid, personid) {
+    return peoplestore.add({ project: projid, person: personid });
+}
+
+function getProjectTeam(id) {
+    return peoplestore.find({ project: id });
+}
+
 module.exports = {
     addProject: addProject,
-    getProjectById: getProjectById
+    getProjectById: getProjectById,
+    addPersonToProject: addPersonToProject,
+    getProjectTeam: getProjectTeam
 }
 
--- test/project.js
+++ test/project.js
@@ -17,3 +17,21 @@ exports['get project by id'] = function (test) {
     test.equal(result.name, 'liqueedid');
     test.equal(result.id, liqueedid);
 };
+
+exports['get people in empty project'] = function (test) {
+    var result = service.getProjectTeam(liqueedid);
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 0);
+};
+
+exports['add person and get people in project'] = function (test) {
+    service.addPersonToProject(liqueedid, 1);
+    var result = service.getProjectTeam(liqueedid);
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 1);
+    test.equal(result[0].person, 1);
+};
