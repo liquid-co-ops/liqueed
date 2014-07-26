71ae4285c668e285bc3cdbf66be5e662c2732cc4
ajlopez <webmaster@ajlopez.com>
2014-07-26 08:33:40 -0300

    Project view

--- routes/project.js
+++ routes/project.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/project');
 
-/* GET home page. */
 router.get('/', controller.index);
+router.get('/:id', controller.view);
 
 module.exports = router;
