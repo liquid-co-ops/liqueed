c6f8626560d938d938eaa793106599bd6f6ce8a0
Nicolas Suarez <nicolassuarez.cabrera@gmail.com>
2014-07-27 14:23:54 -0300

    Angular setup

---
+++ public/scripts/application/app.js
@@ -0,0 +1,19 @@
+var liqueedApp = angular.module('LiqueedApp', ['ngCookies', 'ngResource', 'ngRoute']);
+
+liqueedApp.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  // $locationProvider.html5Mode(true);
+
+  $routeProvider
+    .when('/', {
+      templateUrl: rootApplication('views/home/index.html'),
+      controller: 'HomeCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+}]);
+
+function rootApplication(path){
+  return 'scripts/application/' + path
+}
---
+++ public/scripts/application/controllers/home_ctrl.js
@@ -0,0 +1,8 @@
+
+liqueedApp.controller('HomeCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+
+    $scope.title = 'Liqueed'
+
+  }]
+);
