61bae773fd7e4302b9109d36a144df33ada08634
ajlopez <webmaster@ajlopez.com>
2014-08-23 07:33:23 -0300

    db wrapper, baby step

--- utils/db.js
+++ utils/db.js
@@ -4,17 +4,27 @@ var ostore = require('ostore');
 
 var stores = { };
 
+function Store(impl) {
+    this.get = function (id) { return impl.get(id); }
+    this.find = function (query, projection) { return impl.find(query, projection); }
+    this.add = function (data) { return impl.add(data); }
+    this.put = function (id, data) { return impl.put(id, data); }
+    this.remove = function (id) { return impl.remove(id); }
+    this.update = function (id, data) { return impl.update(id, data); }
+    this.clear = function () { return impl.clear(); }
+}
+
 function getCreateStore(name) {
     if (stores[name])
         return stores[name];
         
-    var store = ostore.createStore();
+    var store = new Store(ostore.createStore());
     stores[name] = store;
     return store;
 }
 
 function createStore(name) {
-    var store = ostore.createStore();
+    var store = new Store(ostore.createStore());
     stores[name] = store;
     return store;
 }
