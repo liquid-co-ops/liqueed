3e0ab45e5d9b1831153ac62b3befa0af3f9055a3
ajlopez <webmaster@ajlopez.com>
2014-09-14 10:19:37 -0300

    Actions style for buttons

--- routes/project.js
+++ routes/project.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var controller = require('../controllers/project');
 
 router.get('/', controller.index);
+router.get('/new', controller.newProject);
+router.post('/new', controller.addProject);
 router.get('/:id', controller.view);
 router.get('/:id/period/:idp', controller.viewPeriod);
 
