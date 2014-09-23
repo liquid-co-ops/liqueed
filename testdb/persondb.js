d45b12ea800fd7d967fc16ba5259a5611254b59c
ajlopez <webmaster@ajlopez.com>
2014-09-23 09:24:48 -0300

    Refactor db tests

--- testdb/persondb.js
+++ testdb/persondb.js
@@ -1,10 +1,5 @@
 
-var service = require('../services/person');
-var pservice = require('../services/project');
-
 var db = require('../utils/db');
-
-var aliceid;
 
 exports['use db'] = function (test) {
     test.async();
@@ -16,77 +11,10 @@ exports['use db'] = function (test) {
     });
 }
 
-exports['add person'] = function (test) {
-    test.async();
-    
-    service.addPerson({ name: 'Alice' }, function (err, result) {
-        test.ok(!err);
-        test.ok(result);
-        aliceid = result;
-        test.done();
-    });    
-};
+var person = require('../test/person');
 
-exports['get person by id'] = function (test) {
-    test.async();
-    
-    service.getPersonById(aliceid, function (err, result) {
-        test.ok(!err);
-        test.ok(result);
-        test.equal(result.name, 'Alice');
-        test.equal(result.id, aliceid);
-        test.done();
-    });    
-};
-
-exports['get persons'] = function (test) {
-    test.async();
-    
-    service.getPersons(function (err, result) {
-        test.ok(!err);
-        test.ok(result);
-        test.ok(Array.isArray(result));
-        test.ok(result.length);
-        test.done();
-    });
-};
-
-exports['no projects yet'] = function (test) {
-    test.async();
-    
-    service.getProjects(aliceid, function (err, result) {
-        test.ok(!err);
-        test.ok(result);
-        test.ok(Array.isArray(result));
-        test.equal(result.length, 0);
-        test.done();
-    });    
-};
-
-exports['add and get projects'] = function (test) {
-    test.async();
-    
-    pservice.addProject({ name: 'Wonderland' }, function (err, projid) {
-        test.ok(!err);
-        test.ok(projid);
-        
-        pservice.addPersonToTeam(projid, aliceid, function (err, pid) {
-            test.ok(!err);
-            test.ok(pid);
-            
-            service.getProjects(aliceid, function (err, result) {
-                test.ok(!err);
-                test.ok(result);
-                test.ok(Array.isArray(result));
-                test.equal(result.length, 1);
-                
-                test.equal(result[0].id, projid);
-                test.equal(result[0].name, 'Wonderland');
-                test.done();
-            });
-        });        
-    });
-}
+for (var n in person)
+    exports[n] = person[n];
 
 exports['close db'] = function (test) {
     test.async();
@@ -97,3 +25,5 @@ exports['close db'] = function (test) {
         test.done();
     });
 }
+
+
