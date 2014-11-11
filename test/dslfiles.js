4b0641ec25f49371fdb1f73332a92b63cadea68a
ajlopez <webmaster@ajlopez.com>
2014-11-11 09:59:40 -0300

    DSL files directory process

--- test/dslfiles.js
+++ test/dslfiles.js
@@ -3,20 +3,49 @@ var dsl = require('./libs/dsl');
 var async = require('simpleasync');
 var db = require('../utils/db');
 var path = require('path');
+var fs = require('fs');
 
-exports['no shares'] = function (test) {
+exports['dsl files'] = function (test) {
     test.async();
+    
+    var pathname = path.join(__dirname, 'dslfiles');
+    
+    fs.readdir(pathname, function (err, filenames) {
+        processfiles();
+        
+        function processfiles() {
+            if (!filenames.length) {
+                test.done();
+                return;
+            }
+            
+            var filename = filenames.shift();
+            
+            processfile(filename, function (err, data) {
+                if (err)
+                    console.log(err);
+                else
+                    setImmediate(processfiles);
+            });
+        }
+    });
+}
+
+function processfile(filename, cb) {
+    console.log();
+    console.log(filename);
     
     async()
     .then(function (data, next) { db.clear(next); })
     .then(function (data, next) {
-        var filename = path.join(__dirname, 'dslfiles', 'noshares.txt');
-        console.log();
-        console.log('noshares.txt');
-        dsl.executeFile(filename, { verbose: true }, next);
+        var fname = path.join(__dirname, 'dslfiles', filename);
+        dsl.executeFile(fname, { verbose: true }, next);
     })
     .then(function (data, next) {
-        test.done();
+        cb(null, null);
+    })
+    .fail(function (err) {
+        cb(err, null);
     })
     .run();
 }
