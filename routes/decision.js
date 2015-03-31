f822360fbe96a385c0a99604453fd4f5d449c9c9
ajlopez <webmaster@ajlopez.com>
2015-03-31 15:48:49 -0300

    Decision new view, WIP

--- routes/decision.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var express = require('express');
-var router = express.Router();
-var controller = require('../controllers/decision');
-
-router.get('/', controller.index);
-
-module.exports = router;
