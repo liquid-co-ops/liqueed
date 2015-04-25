b0bcc1983a1c9dbbdd04e5fcd4a697fea07f0b89
ajlopez <webmaster@ajlopez.com>
2015-04-25 12:44:51 -0300

    Slack service doPerson

--- services/slack.js
+++ services/slack.js
@@ -1,9 +1,28 @@
 
+var personservice = require('./person');
 
 function doTest(params) {
     return params;
 }
 
+function doPerson(cb) {
+    personservice.getPersons(function (err, data) {
+        if (err) {
+            cb(err, null);
+            return;
+        }
+        
+        var result = [];
+        
+        data.forEach(function (person) {
+            result.push(person.username);
+        });
+        
+        cb(null, result);
+    });
+}
+
 module.exports = {
-    doTest: doTest
+    doTest: doTest,
+    doPerson: doPerson
 }
--- test/slack.js
+++ test/slack.js
@@ -2,6 +2,28 @@
 
 var service = require('../services/slack');
 var async = require('simpleasync');
+var loaddata = require('../utils/loaddata');
+var db = require('../utils/db');
+
+var persons;
+
+exports['clear and load data'] = function (test) {
+    var personService = require('../services/person');
+
+    test.async();
+    
+    async()
+    .then(function (data, next) { db.clear(next); })
+    .then(function (data, next) { loaddata(next); })
+    .then(function (data, next) { personService.getPersons(next); })
+    .then(function (data, next) {
+        persons = data;
+        test.ok(persons);
+        test.ok(persons.length);
+        test.done();
+    })
+    .run();
+};
 
 exports['do test'] = function (test) {
     var result = service.doTest({ name: 'Adam', age: 800 });
@@ -10,4 +32,22 @@ exports['do test'] = function (test) {
     test.equal(typeof result, 'object');
     test.equal(result.name, 'Adam');
     test.equal(result.age, 800);
+}
+
+exports['do person'] = function (test) {
+    test.async();
+    
+    service.doPerson(function (err, result) {
+        test.ok(!err);
+        test.ok(result);
+        test.ok(Array.isArray(result));
+        test.ok(result.length);
+        test.equal(result.length, persons.length);
+        
+        persons.forEach(function (person) {
+            test.ok(result.indexOf(person.username) >= 0);
+        });
+        
+        test.done();
+    });
 }
