b36e17472cadfca3d057618d0a281ea96de69811
ajlopez <webmaster@ajlopez.com>
2015-04-25 13:11:12 -0300

    Slack doSlack in API route

--- routes/slackapi.js
+++ routes/slackapi.js
@@ -4,6 +4,6 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/slackapi');
 
-router.get('/', controller.doTest);
+router.get('/', controller.doSlack);
 
 module.exports = router;
